cbd3123adb1d0baafbb27eca1ece8146128c2393
Rinaldi_Bueno <buenosantiagorinaldi@gmail.com>
2024-04-25 09:07:31 -0400

    Puse el login a funcionar, estba en el main :)

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,38 +1,56 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
+  const [value, setValue] = useState({
+    username: "",
+    clave: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${value.username} Clave: ${value.clave}`);
+  };
+
   return (
     <>
-    <div className="Login">
-        <form action="" className='frm'>
+      <div className="Login">
+        <form action="" className="frm">
+          <h1 className="titu">Login</h1>
 
-            <h1 className='titu'>Login</h1>
-
-            <div className="input-groupt">
-
-                <div className="input-feild">
-                    <div className="lala">
-                    <label htmlFor="">Nombre de Usuario:</label>
-                    <input type="text" />
-                    </div>
-                </div>
-                <div className="input-feild">
-                    <div className="lala">
-                    <label htmlFor="">Contraseña: </label>
-                    <input type="password" />
-                    </div>
-                </div>
-
+          <div className="input-groupt">
+            <div className="input-feild">
+              <div className="lala">
+                <label htmlFor="">Nombre de Usuario:</label>
+                <input
+                  onChange={(event) => {
+                    setValue({ ...value, username: event.target.value });
+                  }}
+                  type="text"
+                />
+              </div>
             </div>
+            <div className="input-feild">
+              <div className="lala">
+                <label htmlFor="">Contraseña: </label>
+                <input
+                  onChange={(event) => {
+                    setValue({ ...value, clave: event.target.value });
+                  }}
+                  type="password"
+                />
+              </div>
+            </div>
+          </div>
 
-            <div className="button">
-                    <button>Enviar</button>
-                </div>
+          <div className="button">
+            <button onClick={handleSubmit}>Enviar</button>
+          </div>
         </form>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
