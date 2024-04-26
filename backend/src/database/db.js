9fcafd4a277555b51617db5ad399479175b23508
unknown <buenosantiagorinaldi@gmail.com>
2024-04-26 12:18:17 -0400

    Db.js y ya

---
+++ backend/src/database/db.js
@@ -0,0 +1,116 @@
+const db = require("mysql");
+const config = require("../config");
+
+//Configuracion de la cadena de conexion
+ 
+const conection = {
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database,
+};
+
+const Dbase = db.createConnection(conection);
+
+Dbase.connect((err) => {
+  if (err) return console.log("Error connexion in the data of base :", err);
+  console.log("connexion is runnig");
+});
+
+Dbase.on("error", (err) => {
+  console.log("Error en la base de datos", err);
+  if (err.code === "PROTOCOL_CONNECTION_LOST") {
+    console.log("Error in the CONNECTION LOST");
+  } else {
+    throw err;
+  }
+});
+
+const list = (table) => {
+  return new Promise((resolve, reject) => {
+    Dbase.query(
+      `SELECT * FROM ${table} WHERE estado = 'activo'`,
+      (err, res) => {
+        err ? reject(err) : resolve(res);
+      }
+    );
+  });
+};
+
+const list_inactive = (table) => {
+  return new Promise((resolve, reject) => {
+    Dbase.query(
+      `SELECT * FROM ${table} WHERE estado = 'inactivo'`,
+      (err, res) => {
+        err ? reject(err) : resolve(res);
+      }
+    );
+  });
+};
+
+const reactivated = (table, id) => {
+  return new Promise((resolve, reject) => {
+    Dbase.query(
+      `update ${table} set estado = 'activo' WHERE id_usuario = ${id};`,
+      (err, res) => {
+        err ? reject(err) : resolve(res);
+      }
+    );
+  });
+};
+
+const search = (table, id) => {
+  return new Promise((resolve, reject) => {
+    Dbase.query(`SELECT * FROM ${table} WHERE id_usuario = ${id}`, (err, res) => {
+      err ? reject(err) : resolve(res);
+    });
+  });
+};
+
+const add = (table, data) => {
+  return new Promise((resolve, reject) => {
+    Dbase.query(`INSERT INTO ${table} SET ?;`, [data], (err, res) => {
+      err ? reject(err) : resolve(res);
+    });
+  });
+};
+
+const update = (table, data, id) => {
+  return new Promise((resolve, reject) => {
+    const sql = `UPDATE ${table} SET ? where id_usuario = ${id};`;
+    Dbase.query(sql, [data], (err, res) => {
+      err ? reject(err) : resolve(res);
+    });
+  });
+};
+
+const delet = (table, id) => {
+  return new Promise((resolve, reject) => {
+    const sql = `update ${table} set activo = 'inactivo' WHERE id =  ${id};`;
+    Dbase.query(sql, (err, res) => {
+      err ? reject(err) : resolve(res);
+    });
+  });
+};
+
+const login = (table, data) => {
+  return new Promise((resolve, reject) => {
+    Dbase.query(
+      `SELECT * FROM ${table} WHERE usuario = '${data.user}' and clave = '${data.clave}' `,
+      (err, res) => {
+        err ? reject(err) : resolve(res);
+      }
+    );
+  });
+};
+
+module.exports = {
+  search,
+  list,
+  list_inactive,
+  reactivated,
+  add,
+  delet,
+  update,
+  login,
+};
